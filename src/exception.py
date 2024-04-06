334811208f10e25d9271d95a318f7283390b7874
unknown <gmhafeez17@gmail.com>
2024-04-06 23:37:23 +0500

    EDA and model training

--- src/exception.py
+++ src/exception.py
@@ -1,14 +1,20 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_details(error_detail: tuple):
-    exc_type, exc_obj, exc_tb = error_detail
+def error_message_details(error , error_detail: sys):
+    __,__,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}].".format(
-        file_name, exc_tb.tb_lineno, str(exc_obj))
+        file_name, exc_tb.tb_lineno, str(error))
     return error_message                                 
 
 class Custom_exception(Exception):
-    def __init__(self, error_detail: tuple):
-        super().__init__(error_message_details(error_detail))
+    def __init__(self, error_message,error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_detail=error_detail)
         
+    def __str__ (self):
+        return self.error_message
+        
+
+ 
