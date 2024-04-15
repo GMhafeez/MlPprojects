c33d99e1ed764c7b1c31fb8977333b375291a183
unknown <gmhafeez17@gmail.com>
2024-04-15 19:32:53 +0500

    hyperparameters

--- src/utils.py
+++ src/utils.py
@@ -26,20 +26,31 @@ def save_object (file_path,obj):
     except Exception as e:
         raise Custom_exception(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)  # Fit the model using training data
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            y_train_pred = model.predict(X_train)  # Make predictions on training data
-            y_test_pred = model.predict(X_test)    # Make predictions on test data
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            train_model_score = r2_score(y_train, y_train_pred)  # Evaluate training performance
-            test_model_score = r2_score(y_test, y_test_pred)      # Evaluate test performance
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            report[model_name] = test_model_score  # Store test performance in the report
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
